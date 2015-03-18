74bd7afc8d3afd79beee625dcc74d3183419a47d
Adrian Kim <adrikim@umich.edu>
2015-03-18 11:52:02 -0400

    Changed most logs to warning level

--- labstats-subscriber.py
+++ labstats-subscriber.py
@@ -6,11 +6,6 @@ import labstatslogger
 import sys, os, time, signal
 from daemon import Daemon
 
-# TODO: if subscriber is daemonized, where should output go?
-# to a log file of some sort would be the canonical answer. 
-# maybe /var/log/labstats/subscriber.log for now. 
-# this'll be a starting point for services that want to consume the data.
-
 directory = "/var/run/labstats/"
 logger = labstatslogger.logger
 
@@ -24,22 +19,31 @@ def clean_quit():
     exit(1)
 
 # Output the json into a log file in /var/log/labstats
-def output_log():
-    if not os.path.exists('var/log/labstats/'):
+def output_log(to_write):
+    if not os.path.exists('/var/log/labstats/'):
         try:
-            os.mkdir('var/log/labstats/')
+            os.mkdir('/var/log/labstats/')
         except OSError as e:
-            verbose_print("Error: could not open /var/log/labstats/. Not sudo/root.")
-            logger.debug("Error: could not open /var/log/labstats/. Not sudo/root.")
+            verbose_print("Error: could not make /var/log/labstats/. Not sudo/root.")
+            logger.warning("Error: could not make /var/log/labstats/. Not sudo/root.")
+            return
+    try:
+        logout = open('/var/log/labstats/subscriber.log', 'w')
+        for line in to_write:
+            logout.write(line)
+        logout.close()
+    except Exception as e:
+        verbose_print(repr(e))
+        verbose_print("Error: could not open subscriber.log. No root access.")
+        logger.warning("Error: could not open subscriber.log. No root access.")
 
 # If collector is killed manually, clean up and quit
 def signal_handler(signal, frame):
-    verbose_print("Caught a termination signal")
-    logger.debug("Caught signal "+str(signal)) # signal 15 is SIGTERM
-    logger.warning("Killed subscriber")
+    verbose_print("Caught a termination signal "+signal)
+    logger.warning("Killed subscriber with signal "+signal)
     clean_quit()
 
-signal.signal(signal.SIGTERM, signal_handler) # activates only when SIGTERM detected
+signal.signal(signal.SIGTERM, signal_handler)
 signal.signal(signal.SIGHUP, signal_handler)
 
 def main():   
@@ -49,44 +53,54 @@ def main():
     subscriber.setsockopt(zmq.SUBSCRIBE,'')
     try:
         subscriber.connect('tcp://localhost:5556')
-    except zmq.ZMQError:
-        verbose_print('Error: port 5556 already in use')
-        logger.warning('Warning: subscriber can\'t start, port 5556 already in use')
+    except zmq.ZMQError as e:
+        verbose_print('Error: could not connect to port 5556, is it already in use? Repr: '+repr(e))
+        logger.warning('Warning: subscriber can\'t start, port 5556 already in use? Repr: '+repr(e))
         clean_quit()
     # Done initializing sockets, begin listening for messages
     while True:
         try:
             verbose_print("Waiting for message...")
-            logger.info("Waiting for message...")
+            
             message = subscriber.recv_json()
+
             verbose_print("Received: ")
             verbose_print(message)
-            logger.info('Done receiving...')
+            logger.warning("Received JSON")
+            
+            # Output log if daemonized.
+            if options.daemon:
+                logger.warning("Dumping JSON into logfile")
+                output_log(json.dumps(message))
+
         except zmq.ZMQError as e:
             verbose_print("ZMQ error encountered: attempting restart...")
             logger.warning("Warning: subscriber encountered ZMQ error, restarting...")
             del context, subscriber
+
+            # TODO: Exponential backoff to be implemented here
             if options.daemon:
-                logger.debug("Restarting subscriber daemon in 5 seconds...")
+                logger.warning("Restarting subscriber daemon in 5 seconds...")
                 daemon.restart() # sleep(5) while restarting
                 del context # just in case?
             else: # non-daemonized restart
                 sys.stdout.flush()
                 time.sleep(5)
                 os.execl(sys.executable, *([sys.executable]+sys.argv))
-        except OSError:
-            verbose_print('Error: was not able to restart subscriber. Exiting...')
-            logger.warning('Warning: was not able to restart subscriber. Exiting...')
-            logger.debug("repr: "+repr(e))
-            clean_quit()
+
         except (KeyboardInterrupt, SystemExit):
             verbose_print('\nQuitting subscriber...')
-            logger.info("Quit subscriber")
+            logger.warning("Quit subscriber")
+            clean_quit()
+        # TODO: this triggers (!) occasionally after daemonized process is killed.
+        # Either this or "detected unhandled exception" by abrt, which can't be fixed.
+        except OSError as e:
+            verbose_print('OSError: failed restart? Repr: '+repr(e)+'. Exiting...')
+            logger.warning('OSError: failed restart? Repr: '+repr(e)+'. Exiting...')
             clean_quit()
         except Exception as e:
             verbose_print("Generic exception caught")
-            logger.warning("Caught general exception in subscriber")
-            logger.debug("Repr:"+repr(e))
+            logger.warning("Caught general exception in subscriber, repr:"+repr(e))
 
 class subscriberDaemon(Daemon):
     def run(self):
@@ -109,8 +123,7 @@ if __name__ == '__main__':
             try:
                 os.mkdir(directory)
             except OSError as e: # bad directory, or no permissions
-                logger.error("Encountered OSError while trying to create "+directory)
-                logger.debug("repr: "+repr(e))
+                logger.error("Encountered OSError while trying to create "+directory+", repr: "+repr(e))
                 exit(1)
         daemon = subscriberDaemon(directory+'subscriber.pid')
         daemon.start()
