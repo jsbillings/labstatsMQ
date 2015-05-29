e5dd071dcfea25a093d613b3163e86d3a5053dfc
Adrian Kim <adrikim@umich.edu>
2015-05-29 17:04:03 -0400

    Begin adding request capabilities to hostinfo

--- hostinfo.py
+++ hostinfo.py
@@ -14,45 +14,40 @@ DISP = "Disp"
 LAST_REPORT = "Last Report"
 TEMP = "/tmp"
 ######################
-check_ins = hostinfoclient.check_ins # a copy of the full list
+# TODO: this needs to communicate with hostinfo client to get check_ins
 
 def printheader():
+    # TODO: may be omitted or changed based on other options later
     print "The current time is:", time.asctime(time.localtime()) # eg. Thu May 21 13:25:25 2015
     print "Host name           Type  Edition      Load Ses Disp Last Report   /tmp"
     print "------------------- ----- ----------- ----- --- ---- ------------- --------"
 
 def printitem(json):
-    # hostname, type, edition, cpu load, signed in, (disp?), time, /tmp space
+    # hostname, type, edition, cpu load, sessioncount( number signed in ), (disp?), time checked in, /tmp space
     print '%-19.19s %-5s %-11s%6.2f %3s %-4s %13s %d' % (json["hostname"], json["os"], json["edition"], 
                                                          json["cpuLoad5"], json["userCount"], json["userAtConsole"], 
                                                          json["clientTimestamp"], json["memPhysUsed"])
-    # TODO: what is Display? where to get /tmp? need to convert time to localtime
+    # TODO: what is Display? /tmp is free space in the dir.  need to convert time to localtime
     
-def sift():
-    nitems = 0
-    machinelist = []
-    for host, value in check_ins.iteritems():
-        if not options.all and nitems >= 9:
-            break
-        if options.linux and value['os'] == "Linux":
-            machinelist.push(value)
-            ++nitems
-            continue
-        if options.windows and value['os'] == "Windows":
-            machinelist.push(value)
-            ++nitems
-            continue
-        machinelist.push(value)
-        ++nitems
-    return machinelist
-
-def main():
+def sift(check_ins):
+    machinelist = check_ins.items() # convert dict to list of jsons
+    if options.linux:
+        machinelist = [item for item in machinelist if item["os"] == "Linux"]
+    if options.windows:
+        machinelist = [item for item in machinelist if item["os"] == "Windows"]
+    ###########
+    if options.all:
+        return machinelist # return all
+    return machinelist[:10] # return first 10 items
+    
+def main(check_ins):
     printheader()
-    toprint = sift()
+    toprint = sift(check_ins)
     for d in toprint:
         printitem(d)
 
 if __name__ == "__main__":
+    # All arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--linux', '-l', action='store_true', default=False, dest="linux",
                         help='Request only Linux workstations')
@@ -62,8 +57,21 @@ if __name__ == "__main__":
                         help='Request only Windows workstations')
     options = parser.parse_args()
 
+    # Argument conflict resolution
     if options.linux and options.windows:
         options.linux = False
         print "Warning: --win overrides --linux"
     
-    main()
+    # TODO- Get check_ins, request it from hostinfoclient
+    context = zmq.Context()
+    requester = context.socket(zmq.REQ)
+    requester.bind('tcp://*:5558')
+    try:
+        check_ins = requester.recv_json()
+    except zmq.ZMQError as e:
+        print "Error: hostinfo-client not connected, unable to get labstats data"
+        exit(1)
+    # Alternative: import a list of jsons instead to test out for now?
+    
+    # Begin sifting and printout of data
+    main(check_ins)
