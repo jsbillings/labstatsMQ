a03793035c9171091828496ee42c6d23b29ef6f9
Adrian Kim <adrikim@umich.edu>
2015-06-16 15:17:17 -0400

    Add rudimentary logging

--- hostinfosvc.py
+++ hostinfosvc.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 import zmq, json
-import time, os, sys, signal, logging
+import time, os, sys, signal
 sys.dont_write_bytecode = True
 from multiprocessing import Process, Manager
 import cPickle, zlib
 from daemon import Daemon
 import argparse
+import labstatslogger
 
 # Note: add multiprocessing logger? Regular logging may report as separate processes
+logger = labstatslogger.logger
 # TODO: make sure manager shares check_ins properly
 
 # If  killed manually, clean up and quit
 def sigterm_handler(signal, frame):
-    verbose_print("Caught a SIGTERM")
-    logger.warning("Collector killed via SIGTERM")
+    print "Caught a SIGTERM"
+    logger.warning("Hostinfo service killed via SIGTERM")
     clean_quit()
 
 # If SIGHUP received, do "soft restart" of sockets and files
 def sighup_handler(signal, frame):
-    verbose_print("Caught a SIGHUP")
-    logger.warning("Collector received a SIGHUP")
+    print "Caught a SIGHUP"
+    logger.warning("Hostinfo service received a SIGHUP")
     context.destroy()
     time.sleep(5)
     main()
@@ -38,15 +40,18 @@ def send_data(context, check_ins):
         sender.bind('tcp://*:5558')
     except zmq.ZMQError as e:
         print "Error: unable to bind to port 5558."
+        logger.warning("Error: unable to bind to port 5558."+str(e))
         sys.exit()
     while True:
         try:
             print "Waiting for request..."
             sender.recv()
             print "Received request for data"
+            logger.warning("Received request for data")
         except Exception as e:
             print "Error while receiving request in sender: ", str(e) 
-        # operation cannot be accomplished in current state
+            logger.warning("Error while receiving request in sender: "+str(e))
+            # operation cannot be accomplished in current state
         try:
             # Pickles and compresses data to send to hostinfo
             pickled = cPickle.dumps(check_ins.copy()) # pickles type dict
@@ -55,6 +60,7 @@ def send_data(context, check_ins):
             print "Sent zipped pickled data"
         except Exception as e:
             print "Error while sending data in sender: ", str(e)
+            logger.warning("Error while sending data in sender: "+str(e))
 
 def pull_data(context, check_ins):
     client = context.socket(zmq.PULL)
@@ -62,20 +68,24 @@ def pull_data(context, check_ins):
         client.bind('tcp://*:5557')
     except zmq.ZMQError as e:
         print "Error: unable to bind to port 5557."
+        logger.warning("Error: unable to bind to port 5557."+str(e))
         sys.exit()
     while True:
         try:
             print 'Listening...'
             data = client.recv_json()
             print 'Received message'
+            logger.warning("Received message")
         except Exception as e:
             print "Error while receiving message in puller: ", str(e)
+            logger.warning("Error while receiving message in puller: "+str(e))
         try:
             # Store only successful reads (?)
             if data['success'] is True:
                 check_ins[data["hostname"]] = data # hostname-json pair
         except Exception as e:
             print "Error while adding data in puller: ", str(e)
+            logger.warning("Error while adding data in puller: "+str(e))
 
 def main():
     context = zmq.Context()
@@ -88,6 +98,8 @@ def main():
     except (KeyboardInterrupt, SystemExit): # catch C^c
         sender.terminate()
         context.destroy()
+        if options.daemon:
+            daemon.delpid()
         print "\nQuitting..."
         exit(0)
 
